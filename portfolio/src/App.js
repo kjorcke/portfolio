c995f75a7f7c93c8db393bf98cac2c36cbb27b56
kjorcke <k.jorcke@gmail.com>
2021-04-08 19:04:20 +0200

    overlapping divs and starting with the general layout

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import profilpic from './img/Bewerbungsfoto Januar Quadrat.jpg';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app">
+
+      <div className="spa-container">
+      <div className="navbar">
+          
+          </div>
+    
+          <div className="section1">
+    
+              <div className="image-container">  
+                {/* <img className="profilpic" src={profilpic} alt="profilpic"/> */}
+              </div>
+    
+              <div className="title-container">
+                <div className="name">
+                  Kathleen Jorcke
+                </div>
+              </div>
+    
+          </div>
+      </div>
+
     </div>
   );
 }
